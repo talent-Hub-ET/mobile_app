b5ff6930676bb21876bd388ee3967aaf32f6c052
natyyG <natnalecs16@gmail.com>
2023-12-30 01:23:29 -0800

    course page structure created

--- lib/Pages/CourseSection.dart
+++ lib/Pages/CourseSection.dart
@@ -3,73 +3,70 @@ import 'package:flutter/material.dart';
 class CourseSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Colors.green, // Change the background color as needed
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-              blurRadius: 7,
-              offset: Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/sample_course_image_1.jpg', // Replace with your course image
-              width: 80.0,
-              height: 80.0,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(width: 16.0),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Introduction to Flutter', // Replace with your course name
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.green, // Change the background color as needed
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/sample_course_image_1.jpg', // Replace with your course image
+            width: 80.0,
+            height: 80.0,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(width: 16.0),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.white,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Introduction to Flutter', // Replace with your course name
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      'Hours Taken: 5 hours', // Replace with actual hours taken
-                      style: TextStyle(
-                        fontSize: 14.0,
-                      ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'Hours Taken: 5 hours', // Replace with actual hours taken
+                    style: TextStyle(
+                      fontSize: 14.0,
                     ),
-                    Text(
-                      'Lessons Left: 25 lessons', // Replace with actual lessons left
-                      style: TextStyle(
-                        fontSize: 14.0,
-                      ),
+                  ),
+                  Text(
+                    'Lessons Left: 25 lessons', // Replace with actual lessons left
+                    style: TextStyle(
+                      fontSize: 14.0,
                     ),
-                    SizedBox(height: 12.0),
-                    _ProgressBar(
-                        progress:
-                            70), // Replace with actual progress percentage
-                  ],
-                ),
+                  ),
+                  SizedBox(height: 12.0),
+                  _ProgressBar(
+                      progress: 70), // Replace with actual progress percentage
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -83,31 +80,30 @@ class _ProgressBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 20.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: Colors.grey.withOpacity(0.5),
-      ),
-      child: Stack(
-        children: [
-          Container(
-            width: double.infinity * (progress / 100),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: Colors.blue, // Change the progress bar color as needed
-            ),
-          ),
-          Center(
-            child: Text(
-              '$progress%', // Display the progress percentage in the middle
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+        height: 20.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.grey.withOpacity(0.5),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity * (progress / 100),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.blue, // Change the progress bar color as needed
               ),
             ),
-          ),
-        ],
-      ),
-    );
+            Center(
+              child: Text(
+                '$progress%', // Display the progress percentage in the middle
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
--- lib/Pages/CoursesPage.dart
+++ lib/Pages/CoursesPage.dart
@@ -77,6 +77,435 @@ class CoursePage extends StatelessWidget {
           ],
         ),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // User Profile Section
+              Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30.0,
+                      backgroundImage:
+                          AssetImage('assets/sample_profile_picture.jpg'),
+                    ),
+                    SizedBox(width: 16.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Welcome!',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        Text(
+                          'John Doe',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              // New Courses Section
+              Text(
+                'New Courses',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                height: 200.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    _CourseCard(
+                      image: 'assets/sample_course_image_1.jpg',
+                      title: 'Introduction to Flutter',
+                      hours: '10 hours',
+                      lessons: '30 lessons',
+                      buttonText: 'Get Started',
+                    ),
+                    SizedBox(width: 16.0),
+                    _CourseCard(
+                      image: 'assets/sample_course_image_2.jpg',
+                      title: 'Web Development Basics',
+                      hours: '8 hours',
+                      lessons: '25 lessons',
+                      buttonText: 'Keep Learning',
+                    ),
+                    SizedBox(width: 16.0),
+                    _CourseCard(
+                      image: 'assets/sample_course_image_3.jpg',
+                      title: 'Machine Learning Fundamentals',
+                      hours: '12 hours',
+                      lessons: '40 lessons',
+                      buttonText: 'Get Started',
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              // Top Mentors Section
+              Text(
+                'Top Mentors',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                height: 120.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    _MentorCard(
+                      image: 'assets/sample_mentor_picture_1.jpg',
+                      name: 'Alice Johnson',
+                      profession: 'Web Developer',
+                      courses: '5 Courses',
+                    ),
+                    SizedBox(width: 16.0),
+                    _MentorCard(
+                      image: 'assets/sample_mentor_picture_2.jpg',
+                      name: 'Bob Smith',
+                      profession: 'Data Scientist',
+                      courses: '8 Courses',
+                    ),
+                    SizedBox(width: 16.0),
+                    _MentorCard(
+                      image: 'assets/sample_mentor_picture_3.jpg',
+                      name: 'Eva Williams',
+                      profession: 'UI/UX Designer',
+                      courses: '3 Courses',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CourseCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String hours;
+  final String lessons;
+  final String buttonText;
+
+  const _CourseCard({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.hours,
+    required this.lessons,
+    required this.buttonText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10.0),
+              topRight: Radius.circular(10.0),
+            ),
+            child: Image.asset(
+              image,
+              height: 120.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 4.0),
+                Text(
+                  '$hours • $lessons',
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle button press
+                  },
+                  child: Text(buttonText),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MentorCard extends StatelessWidget {
+  final String image;
+  final String name;
+  final String profession;
+  final String courses;
+
+  const _MentorCard({
+    Key? key,
+    required this.image,
+    required this.name,
+    required this.profession,
+    required this.courses,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10.0),
+              topRight: Radius.circular(10.0),
+            ),
+            child: Image.asset(
+              image,
+              height: 80.0,
+              width: 150.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 4.0),
+                Text(
+                  profession,
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: 4.0),
+                Text(
+                  courses,
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle button press
+                  },
+                  child: Text('See Class'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CourseSection(),
+    );
+  }
+}
+
+class CourseSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.green, // Change the background color as needed
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/sample_course_image_1.jpg', // Replace with your course image
+              width: 80.0,
+              height: 80.0,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(width: 16.0),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Introduction to Flutter', // Replace with your course name
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'Hours Taken: 5 hours', // Replace with actual hours taken
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    Text(
+                      'Lessons Left: 25 lessons', // Replace with actual lessons left
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    SizedBox(height: 12.0),
+                    _ProgressBar(
+                        progress:
+                            70), // Replace with actual progress percentage
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ProgressBar extends StatelessWidget {
+  final int progress;
+
+  const _ProgressBar({Key? key, required this.progress}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.grey.withOpacity(0.5),
+      ),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity * (progress / 100),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.blue, // Change the progress bar color as needed
+            ),
+          ),
+          Center(
+            child: Text(
+              '$progress%', // Display the progress percentage in the middle
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
